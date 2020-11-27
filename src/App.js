86bafcc7bb0507d25edc5f3bb06acdcbe9c93a95
Alex Nirenovic <a.nirenovic@gmail.com>
2020-11-28 08:50:39 +1100

    Demo front page updates
    Progress to the placeholder appearance of the front page.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,69 @@ function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
+				<div className="header-inner">
+					<a href="/" id="logo">Scamazon.</a>
+				</div>
 			</header>
 			<main>
-				
+				<div className="grid">
+					<div className="card">
+						<a href="">
+							<div className="card-image-wrapper">
+								<div className="card-image" style={{backgroundImage: "url(./images/gameboy.jpg)"}}></div>
+							</div>
+							<div className="card-text-wrapper">
+								<div className="card-text">
+									<span className="card-title">Yellow Gameboy</span>
+									<span className="card-price">$72.00</span>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div className="card">
+						<a href="">
+							<div className="card-image-wrapper">
+								<div className="card-image" style={{backgroundImage: "url(./images/gameboy.jpg)"}}></div>
+							</div>
+							<div className="card-text-wrapper">
+								<div className="card-text">
+									<span className="card-title">Yellow Gameboy</span>
+									<span className="card-price">$72.00</span>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div className="card">
+						<a href="">
+							<div className="card-image-wrapper">
+								<div className="card-image" style={{backgroundImage: "url(./images/gameboy.jpg)"}}></div>
+							</div>
+							<div className="card-text-wrapper">
+								<div className="card-text">
+									<span className="card-title">Yellow Gameboy</span>
+									<span className="card-price">$72.00</span>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div className="card">
+						<a href="">
+							<div className="card-image-wrapper">
+								<div className="card-image" style={{backgroundImage: "url(./images/gameboy.jpg)"}}></div>
+							</div>
+							<div className="card-text-wrapper">
+								<div className="card-text">
+									<span className="card-title">Yellow Gameboy</span>
+									<span className="card-price">$72.00</span>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
 			</main>
+			<footer>
+
+			</footer>
 		</div>
 	);
 }
