05a3b49e18fa1c7a34c2544523fd3be84226cb72
Alex Nirenovic <a.nirenovic@gmail.com>
2020-11-30 10:11:06 +1100

    Redux code and backend data fetching for product details

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,27 +1,46 @@
-import React from 'react';
-import Product from '../components/product';
-import data from '../data';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { detailsProduct } from '../actions/productActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import Product from '../components/Product';
 
 export default function ProductScreen(props) {
-    const product = data.products.find((x) => x._id === props.match.params.id);
+    const dispatch = useDispatch();
+    const productId = props.match.params.id;
+    const productDetails = useSelector( state => state.productDetails);
+    const {loading, error, product} = productDetails;
+
+    useEffect(() => {
+        dispatch(detailsProduct(productId));
+    }, [dispatch, productId]);
+
     return (
-        <div className="product-wrapper content-block">
-            <div className="product-main">
-                <Product key={product._id} product={product}></Product>
-                <p className="product-description">
-                    {product.description}
-                </p>
-            </div>
-            <div className="product-buy">
-                {product.count > 0 ? (
-                    <div>
-                        <span className="success">{product.count} currently in stock.</span>
-                        <button>Add to Cart</button>
-                    </div>
+        <div>
+            {loading ? (
+                <LoadingBox></LoadingBox>
+                ) : error ? (
+                    <MessageBox variant="error">{error}</MessageBox>
                 ) : (
-                    <span className="error">Product currently unavailable.</span>
+                    <div className="product-wrapper content-block">
+                        <div className="product-main">
+                            <Product key={product._id} product={product}></Product>
+                            <p className="product-description">
+                                {product.description}
+                            </p>
+                        </div>
+                        <div className="product-buy">
+                            {product.count > 0 ? (
+                                <div>
+                                    <span className="success">{product.count} currently in stock.</span>
+                                    <button>Add to Cart</button>
+                                </div>
+                            ) : (
+                                <span className="error">Product currently unavailable.</span>
+                            )}
+                        </div>
+                    </div>
                 )}
-            </div>
         </div>
     )
 }
